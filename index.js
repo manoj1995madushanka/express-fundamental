96cd959c5863b6fae7f03385647113224fb553a6
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-10-13 09:13:39 +0530

    get endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express')
 
 app = express();
 
+app.get("/",function (req,res){
+    res.send("Hello express")
+})
+
 app.listen(8000, function () {
     console.log('Serve run successfully')
 })
