84724be3edb0c7606103eeecbbaf2ee948cb497a
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-10-13 18:19:38 +0530

    multiple endpoints

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ var express = require('express')
 app = express();
 
 app.get("/",function (req,res){
-    res.send("Hello express")
+    res.send("Home page")
+})
+
+app.get("/about",function (req,res){
+    res.send("About page")
+})
+
+app.get("/contact",function (req,res){
+    res.send("Contact page")
 })
 
 app.listen(8000, function () {
